69bdd3ba01239a11f1b40963d975d2f934ed6927
abigailwambui <abigailw15njuguna@gmail.com>
2019-05-20 10:41:57 +0300

    save changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -74,14 +74,14 @@ public class App {
         }, new VelocityTemplateEngine());
 
         post("/tasks", (request, response) -> {
-        Map<String, Object> model = new HashMap<String, Object>();
-        Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
-        String description = request.queryParams("description");
-        Task newTask = new Task(description);
-        category.addTask(newTask);
-        model.put("category", category);
-        model.put("template", "templates/category-tasks-success.vtl");
-        return new ModelAndView(model, layout);
+            Map<String, Object> model = new HashMap<String, Object>();
+            Category category = Category.find(Integer.parseInt(request.queryParams("categoryId")));
+            String description = request.queryParams("description");
+            Task newTask = new Task(description);
+            category.addTask(newTask);
+            model.put("category", category);
+            model.put("template", "templates/category-tasks-success.vtl");
+            return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
      ProcessBuilder process = new ProcessBuilder();
